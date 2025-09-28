b331a54c860d8488ad8d302d91f5824069838b15
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2025-09-28 07:56:02 +0700

    :bug: reconfig vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
+    tailwindcss(),
   ],
 
   // Set cho admin chay o port 5174 khac cong mac dinh cua vite 5173
